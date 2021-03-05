c2a87e70008e6d8d28a238d70fcaf26c49c864ce
shrutikumar Dalvi <shrdalvi@cisco.com>
2021-03-05 15:05:13 +0530

    All Files

---
+++ src/main/java/com/techprimers/serverless/Hello.java
@@ -0,0 +1,25 @@
+package com.techprimers.serverless;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class Hello implements Function<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(Hello.class);
+	
+    @Override
+    public APIGatewayProxyResponseEvent apply(APIGatewayProxyRequestEvent input) {
+        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+        LOGGER.info(input.getBody());
+        responseEvent.setStatusCode(200);
+        responseEvent.setBody("Hello! Reached the Spring Cloud Function with message: " + input.getBody());
+        return responseEvent;
+    }
+}
